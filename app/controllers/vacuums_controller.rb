4558cebafc3997bee8cf51f6d38cc8e3a1f46793
yfern328 <yfernan2@u.rochester.edu>
2017-07-18 09:56:32 -0400

    final touchups

--- app/controllers/vacuums_controller.rb
+++ app/controllers/vacuums_controller.rb
@@ -20,7 +20,7 @@ class VacuumsController < ApplicationController
     return redirect_to signup_path if !logged_in
     @user = current_user
     @reviews = Review.all.select {|review| review.vacuum_id == @vacuum.id}
-    if !Vacuum.all.empty?
+    if !@reviews.empty?
       ratings = @reviews.map {|review| review.emoji_rating.to_i}
       @average_rating = ratings.reduce(:+) / ratings.length
     end
